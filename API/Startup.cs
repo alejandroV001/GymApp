a18029c38e169729c8a3aa2093e59e0e577af8a6
AlejandroVE01 <alyvasilescu@gmail.com>
2022-03-29 21:07:38 +0300

    Second

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,7 @@ namespace API
                 options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -51,6 +52,11 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy =>{
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+            });
+
 
             app.UseAuthorization(); 
 
