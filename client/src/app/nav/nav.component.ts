3833bbf4f2746a4949bfd99c483746e89284ef8c
AlejandroVE01 <alyvasilescu@gmail.com>
2022-04-01 00:29:03 +0300

    Four

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  model:any={}
 
-  constructor() { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    this.accountService.login(this.model).subscribe(response =>{
+      console.log(response);
+    }, error =>{
+      console.log(error);
+    })
+  }
+
+  logout(){
+    this.accountService.logout();
   }
 
 }
